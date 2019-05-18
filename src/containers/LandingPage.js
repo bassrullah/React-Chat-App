f2918ba6111d39decd32a71b8a5b7b66590bdca5
bassrullah <bassrullah@gmail.com>
2019-05-18 13:15:43 +0700

    [Bass] - install package react-router-dom, create landing page, login & register

---
+++ src/containers/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Login from '../components/Login.js'
+
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <Login/>
+      </div>
+    )
+  }
+}
